4ce60768db4860ac16543d1066ea7a5ebf56454e
Lucas <108193942+lucasrosa0609@users.noreply.github.com>
2022-09-05 13:41:18 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,5 @@
+👋 Hi, I’m @lucasrosa0609. I'm a lawyer interested in technology
+👀 I’m interested in programing, general
+🌱 I’m currently learning Python
+💞️ I’m looking to collaborate on everything that i can help
+📫 How to reach me: lusofero@gmail.com
